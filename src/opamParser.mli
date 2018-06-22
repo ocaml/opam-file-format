d60cb997d4d0ca1fc8458a89dc194d023cc29576
David Allsopp <david.allsopp@metastack.com>
2018-06-22 15:37:32 +0100

    API Documentation

--- src/opamParser.mli
+++ src/opamParser.mli
@@ -8,13 +8,16 @@
 (*                                                                        *)
 (**************************************************************************)
 
+(** opam format parser *)
+
 open OpamParserTypes
 
-(** Raw OpamBaseParser entry points;
+(** {2 Raw OpamBaseParser entry points } *)
 
-    Providing a custom [lexbuf] argument allows you, for example, to
-    set the initial lexing position. For the first argument, you may
-    use the {!OpamLexer.token} lexing function:
+(**
+    Providing a custom [lexbuf] argument allows you, for example, to set the
+    initial lexing position. For the first argument, you may use the
+    {!OpamLexer.token} lexing function:
 
 {[
     let lexbuf = Lexing.from_string input in
@@ -22,19 +25,38 @@ open OpamParserTypes
     OpamParser.value OpamLexer.token lexbuf
 ]}
 *)
+
 val main:
   (Lexing.lexbuf  -> OpamBaseParser.token) ->
   Lexing.lexbuf -> file_name -> opamfile
+(** Principal parser: given a lexbuf and the filename it was read from, returns
+    an {!OpamParserTypes.opamfile} record parsed from it. *)
+
 val value:
   (Lexing.lexbuf  -> OpamBaseParser.token) ->
   Lexing.lexbuf -> value
+(** Lower-level function just returning a single {!OpamParserTypes.value} from
+    a given lexer. *)
 
-(** file parsers *)
+(** {2 File parsers } *)
 val string: string -> file_name -> opamfile
+(** Parse the content of a file already read to a string. Note that for
+    CRLF-detection to work on Windows, it is necessary to read the original file
+    using binary mode on Windows! *)
 val channel: in_channel -> file_name -> opamfile
+(** Parse the content of a file from an already-opened channel. Note that for
+    CRLF-detection to work on Windows, it is necessary for the channel to be
+    in binary mode! *)
 val file: file_name -> opamfile
+(** Parse the content of a file. The file is opened in binary mode, so
+    CRLF-detection works on all platforms. *)
 
-(** value parsers *)
+(** {2 [value] parsers } *)
 val value_from_string: string -> file_name -> value
+(** Parse the first value in the given string. [file_name] is used for lexer
+    positions. *)
 val value_from_channel: in_channel -> file_name -> value
+(** Parse the first value from the given channel. [file_name] is used for
+    lexer positions. *)
 val value_from_file: file_name -> value
+(** Parse the first value from the given file. *)
